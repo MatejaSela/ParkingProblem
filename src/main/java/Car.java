d3f4a5e9fa3463f200c85e4006734a06c4ad330b
Mateja Sela <mateja.sela@thoughtworks.com>
2020-12-14 09:59:25 +0100

    add car class to create separate car objects with type, money to pay fees, hours stayed at parking lot...

---
+++ src/main/java/Car.java
@@ -0,0 +1,47 @@
+package main.java;
+
+public class Car {
+    String carType;
+    Double moneyToPayFees;
+    Integer hoursStayedAtParkingLot;
+    Boolean enteringTheLot;
+
+    public Car(String carType, Double moneyToPayFees, Integer hoursStayedAtParkingLot, Boolean enteringTheLot) {
+        this.carType = carType;
+        this.moneyToPayFees = moneyToPayFees;
+        this.hoursStayedAtParkingLot = hoursStayedAtParkingLot;
+        this.enteringTheLot = enteringTheLot;
+    }
+
+    public String getCarType() {
+        return this.carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public Double getMoneyToPayFees() {
+        return this.moneyToPayFees;
+    }
+
+    public void setMoneyToPayFees(Double moneyToPayFees) {
+        this.moneyToPayFees = moneyToPayFees;
+    }
+
+    public Integer getHoursStayedAtParkingLot() {
+        return this.hoursStayedAtParkingLot;
+    }
+
+    public void setHoursStayedAtParkingLot(Integer hoursStayedAtParkingLot) {
+        this.hoursStayedAtParkingLot = hoursStayedAtParkingLot;
+    }
+
+    public Boolean getEnteringTheLot() {
+        return this.enteringTheLot;
+    }
+
+    public void setEnteringTheLot(Boolean enteringTheLot) {
+        this.enteringTheLot = enteringTheLot;
+    }
+}

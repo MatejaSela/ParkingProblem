499540a6aa6fe694aeb2381a7f26506ab879b4be
Mateja Sela <mateja.sela@thoughtworks.com>
2020-12-14 10:31:38 +0100

    add two tests for entering and exiting the lot

--- src/main/java/test/ParkingLotStartTest.java
+++ src/main/java/test/ParkingLotStartTest.java
@@ -4,13 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import main.java.Car;
 
 public class ParkingLotStartTest {
-    Car car1 = new Car("FiftyKW", 20.0, 0, true);
-    Car car2 = new Car("Standard", 0.0, 2, true);
-    Car car3 = new Car("TwentyKW", 100.0, 0, true);
+
 
     @Test
-    public void multiplicationOfZeroIntegersShouldReturnZero() {
-        MyClass tester = new MyClass(); // MyClass is tested
+    public void enterParkingLot_ReducesCarsMoneyForEntranceFeeValueAndChecksSpaceForParkingLotCorrectly() {
+        Car car1 = new Car("FiftyKW", 20.0, 0, true);
+        Car car2 = new Car("Standard", 0.0, 2, true);
+        Car car3 = new Car("TwentyKW", 100.0, 0, true);
+
+        ParkingLot parkingLot1 = new ParkingLot();
+
+        // assert statements
+        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    }
+
+    @Test
+    public void exitParkingLot_ReducesCarsMoneyForExitFeeAndIncreasesNumberOfSpacesForParkingLotCorrectly() {
+        Car car1 = new Car("FiftyKW", 20.0, 0, true);
+        Car car2 = new Car("Standard", 0.0, 2, true);
+        Car car3 = new Car("TwentyKW", 100.0, 0, true);
 
         // assert statements
         assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");

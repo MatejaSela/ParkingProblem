0f07066deb180cac2b2d9eac9288cd8e45f1bdcc
Mateja Sela <mateja.sela@thoughtworks.com>
2020-12-14 12:06:01 +0100

    add test for entering parking lot, with exception for no money

--- src/test/java/ParkingLotStartTest.java
+++ src/test/java/ParkingLotStartTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,6 @@ public class ParkingLotStartTest {
     Integer fiftyKWFreeSlotNumber = 1;
     Double fiftyKWSlotPricePerHour = 10.0;
     Double parkingFixedFee = 10.0;
-    Car car1 = new Car("FiftyKW", 20.0, 1, true);
 
     ParkingLot parkingLot1 = new ParkingLot(standardFreeSlotNumber, standardSlotPricePerHour, twentyKWFreeSlotNumber,
             twentyKWSlotPricePerHour, fiftyKWFreeSlotNumber, fiftyKWSlotPricePerHour, parkingFixedFee);
@@ -23,11 +23,33 @@ public class ParkingLotStartTest {
     @Test
     public void enterParkingLot_givenCarWithEnoughMoney_AndParkingLotWithEnoughSpace_ReducesCarsMoneyForEntranceFeeValueAndChecksSpaceForParkingLotCorrectly()
             throws NoMoneyExcetion, NoSpaceExcetion {
+        // Setup
+        Car car = new Car("FiftyKW", 20.0, 1, true);
+
         // Act
-        ParkingLotStart.enterParkingLot(car1, parkingLot1);
+        ParkingLotStart.enterParkingLot(car, parkingLot1);
         // Check
-        assertEquals(20.0 - parkingLot1.parkingFixedFee, car1.moneyToPayFees, 0.1);
+        assertEquals(20.0 - parkingLot1.parkingFixedFee, car.moneyToPayFees, 0.1);
         assertEquals(0, parkingLot1.fiftyKWFreeSlotNumber, 0.1);
+    }
+
+    @Test
+    public void enterParkingLot_givenCarWithNoMoney_ThrowsException() throws NoMoneyExcetion, NoSpaceExcetion {
+        // Setup
+        Car car = new Car("FiftyKW", 0.0, 1, true);
+
+        // Act and Check
+        assertThrows(NoMoneyExcetion.class, () -> ParkingLotStart.enterParkingLot(car, parkingLot1));
+    }
+
+    @Test
+    public void enterParkingLot_givenNotEngoughSpaceAtLot_ThrowsException() throws NoMoneyExcetion, NoSpaceExcetion {
+        // Setup
+        Car car = new Car("FiftyKW", 1000.0, 1, true);
+        parkingLot1.fiftyKWFreeSlotNumber = 0;
+
+        // Act and Check
+        assertThrows(NoSpaceExcetion.class, () -> ParkingLotStart.enterParkingLot(car, parkingLot1));
     }
 
     /*
